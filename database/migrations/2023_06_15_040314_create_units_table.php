aaabc0f241927c0933c2111fdb6f35def6beb17a
lysokheng <sokheng6485@gmail.com>
2023-06-15 22:33:08 +0700

    [FIX] foreign key product id on new table

--- database/migrations/2023_06_15_040314_create_units_table.php
+++ database/migrations/2023_06_15_040314_create_units_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,9 +13,11 @@ return new class extends Migration
         Schema::create('units', function (Blueprint $table) {
             $table->id();
             $table->string('units');
-            $table->integer('product_id')->unsigned();
             $table->timestamps();
-            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('product_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
         });
     }
 
